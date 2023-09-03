e3c9ea492be2a31d97a62251e89a949a29aba114
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-03 16:46:50 -0600

    fixed global variables, framed function

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const electronics = 'Electronics';
-const jewelry = 'Jewelry';
-const menClothing = "Men's Clothing";
-const womenClothing = "Women's Clothing";
+const electronics = document.getElementById('Electronics');
+const jewelry = document.getElementById('Jewelry');
+const menClothing = document.getElementById("Men's Clothing");
+const womenClothing = document.getElementById("Women's Clothing");
 const display = document.getElementById('display');
-const API = "https://fakestoreapie.com/";
+const API = "https://fakestoreapi.com/";
+
+async function fakeStore(endpoint) {
+	
+};
