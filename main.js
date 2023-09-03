aed3d53af80e45fc8740c995c828ae2de248bea8
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-03 16:57:45 -0600

    data function done

--- main.js
+++ main.js
@@ -6,5 +6,7 @@ const display = document.getElementById('display');
 const API = "https://fakestoreapi.com/";
 
 async function fakeStore(endpoint) {
-	
+	let result = await fetch(API + endpoint);
+	let data = await result.json();
+	return data;
 };
