d061db71ccf801119b606708eb3dd8ce968873bd
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-04 19:19:28 -0600

    #U4_03 done with comments

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
+// global references to the categories in the navbar
 const electronics = document.getElementById('Electronics');
 const jewelry = document.getElementById('Jewelry');
 const menClothing = document.getElementById("Men's Clothing");
 const womenClothing = document.getElementById("Women's Clothing");
+
+// global reference to the display
 const display = document.getElementById('display');
+
+// global reference to the API that will be used
 const API = "https://fakestoreapi.com/";
 
+// the four categories in the navbar are assigned their event listeners
+electronics.onclick = function() {
+	console.log(fakeStore("products/category/electronics"))
+};
+
+jewelry.onclick = function() {
+	console.log(fakeStore("products/category/jewelry"))
+};
+
+menClothing.onclick = function() {
+	console.log(fakeStore("products/category/men's clothing"))
+};
+
+womenClothing.onclick = function() {
+	console.log(fakeStore("products/category/women's clothing"))
+};
+
+//returns the promise with information
 const fakeStore = async (endpoint) => {
 	let result = await fetch(API + endpoint);
-	console.log(result);
 	let data = await result.json();
-	console.log(data);
 	return data;
 };
 
-console.log(fakeStore("products/category/jewelery"));
-
-window.onload = function() {console.log(fakeStore("products"))}
+// event listener assigned to onload - returns all products in ascending order
+window.onload = function() {
+	console.log(fakeStore("products?sort=asc"))
+};
