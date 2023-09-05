015da056fd3ffbd80d48395173758a75c67ebb2f
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-09-05 12:28:18 -0600

    cards made, accordions not working

--- main.js
+++ main.js
@@ -41,7 +41,75 @@ const displayCards = (data) => {
 	// create the element
 	// change the element
 	// add the element
-};
+	display.innerHTML = '';
+	let cards = [];
+	data.forEach(product => {
+		cards.push({
+			product: product,
+			body: document.createElement('div'),
+			image: document.createElement('img'),
+			title: document.createElement('div'),
+			accordion: document.createElement('div'),
+			descriptionItem: document.createElement('div'),
+			descriptionHeader: document.createElement('h2'),
+			descriptionButton: document.createElement('button'),
+			descriptionCollapse: document.createElement('div'),
+			descriptionBody: document.createElement('div'),
+			priceItem: document.createElement('div'),
+			priceHeader: document.createElement('h2'),
+			priceButton: document.createElement('button'),
+			priceCollapse: document.createElement('div'),
+			priceBody: document.createElement('div'),
+			addButton: document.createElement('button')
+		});
+	});
+	
+	// creates and appends the parts of the cards
+	cards.forEach(card => {
+		card.body.className = 'card';
+		card.body.style = 'width: 18rem;';
+		card.image.className = 'card-img-top';
+		card.image.src = `${card.product.image}`;
+		card.image.alt = `${card.product.title}`;
+		card.title.innerText = `${card.product.title}`;
+		card.title.className = 'card-title';
+		card.accordion.className = 'accordion';
+		card.descriptionItem.className = 'accordion-item';
+		card.descriptionHeader.className = 'accordion-header';
+		card.descriptionButton.className = 'accordion-button';
+		card.descriptionButton.innerText = 'Description';
+		card.descriptionButton.data
+		card.descriptionCollapse.className = 'accordion-collapse collapse show';
+		card.descriptionBody.className = 'accordion-body';
+		card.descriptionBody.innerText = `${card.product.description}`;
+		card.priceItem.className = 'accordion-item';
+		card.priceHeader.className = 'accordion-header';
+		card.priceButton.className = 'accordion-button';
+		card.priceButton.innerText = 'Price';
+		card.priceCollapse.className = 'accordion-collapse collapse show';
+		card.priceBody.className = 'accordion-body';
+		card.priceBody.innerText = `${card.product.price}`;
+		card.addButton.innerText = 'Add to Cart';
+
+		card.body.append(card.image);
+		card.body.append(card.title);
+		card.body.append(card.accordion);
+		card.accordion.append(card.descriptionItem);
+		card.descriptionItem.append(card.descriptionHeader);
+		card.descriptionHeader.append(card.descriptionButton);
+		card.accordion.append(card.descriptionCollapse);
+		card.descriptionCollapse.append(card.descriptionBody);
+		//
+		card.accordion.append(card.priceItem);
+		card.priceItem.append(card.priceHeader);
+		card.priceHeader.append(card.priceButton);
+		card.accordion.append(card.priceCollapse);
+		card.priceCollapse.append(card.priceBody);
+		card.body.append(card.addButton);
+		display.append(card.body);
+	});
+	console.log(cards);
+}
 
 // event listener assigned to onload - returns all products in ascending order
 window.onload = function() {
